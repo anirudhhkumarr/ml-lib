c1e3a8bc76a6ea88945f1149887d84a95ba6f96f
Raj Engla <rajengla@touchtalent.com>
2013-04-14 02:49:08 +0530

    git ignore file added and tab problem solved

--- src/Input/Input.hs
+++ src/Input/Input.hs
@@ -21,40 +21,40 @@ import Data.Function
 import Data.List
 
 instance Ord AttributeValue where
-   compare (NumericValue a) (NumericValue b) = compare a b
-   compare (NominalValue a) (NominalValue b) = compare a b
+    compare (NumericValue a) (NumericValue b) = compare a b
+    compare (NominalValue a) (NominalValue b) = compare a b
 
 instance Eq AttributeValue where
-   (NumericValue a) == (NumericValue b) =  a == b
-   (NominalValue a) == (NominalValue b) =  a == b
+    (NumericValue a) == (NumericValue b) =  a == b
+    (NominalValue a) == (NominalValue b) =  a == b
 --Classifer
 data AttributeInfo = NUMERIC (Double,Double)
-		     |NOMINAL [(BS.ByteString,Double)]--[(Nominal,count)]
+                    |NOMINAL [(BS.ByteString,Double)]--[(Nominal,count)]
 instance Show AttributeInfo where
-	show (NUMERIC x) = show x
-	show (NOMINAL x) = show x
-
+    show (NUMERIC x) = show x
+    show (NOMINAL x) = show x
+    
 trainData::[Attribute]->[BS.ByteString]->[[AttributeValue]]->[[AttributeInfo]]
-trainData attributesInfo (first:rest) input = (classInfo : (trainData attributesInfo rest restClassData))
-			 							      where
-											    intialInfo = intialize attributesInfo 						
- 						      					(classInfo,restClassData) = trainClass intialInfo first input 0 				      
+trainData attributesInfo (first:rest) input =(classInfo : (trainData attributesInfo rest restClassData))
+                                                where
+                                                    intialInfo = intialize attributesInfo 						
+                                                    (classInfo,restClassData) = trainClass intialInfo first input 0 				      
 trainData _ _ [] = []
 
 intialize::[Attribute]->[AttributeInfo]
 intialize = map foo
 
 foo::Attribute->AttributeInfo
-foo a =  case (dataType a) of Nominal xs -> NOMINAL $ intializeNominal xs
-							  Numeric -> NUMERIC (0.0,0.0)
+foo a =case (dataType a) of Nominal xs -> NOMINAL $ intializeNominal xs
+                            Numeric -> NUMERIC (0.0,0.0)
 
 intializeNominal:: [BS.ByteString]->[(BS.ByteString,Double)]
 intializeNominal = map (\ x->(x,0))
 
 trainClass::[AttributeInfo]->BS.ByteString->[[AttributeValue]]->Double->([AttributeInfo],[[AttributeValue]])
 trainClass info first (x:xs) n 
-	|(last x) == (NominalValue first) = trainClass (updateClassInfo info $ init x) first xs (n+1)
-	| otherwise  = (finalizeInfo info n,x:xs)
+    |(last x) == (NominalValue first) = trainClass (updateClassInfo info $ init x) first xs (n+1)
+    | otherwise  = (finalizeInfo info n,x:xs)
 trainClass info _ [] n = (finalizeInfo info n,[])
 
 parseARFF input = parse arff input    
@@ -65,12 +65,12 @@ main = do
         handle <- openFile "arff.arff" ReadMode
         contents <- hGetContents handle
         let input = case (parseARFF $ pack contents) of Partial k -> (k BS.empty)
-                     			    	        x-> x
-	let (_header,_data) = case input of Done _ y ->y
-	let inputData = map catMaybes _data	
-	    numAttributes = length $ attributes _header
-	    Nominal classes = dataType $ last $ attributes _header
-	    attributesInfo = attributes _header	
-	    sortedInput = sortByClass inputData numAttributes	    
-	print sortedInput	
-	return ()
+                                                        x-> x
+            (_header,_data) = case input of Done _ y ->y
+            inputData = map catMaybes _data	
+            numAttributes = length $ attributes _header
+            Nominal classes = dataType $ last $ attributes _header
+            attributesInfo = attributes _header	
+            sortedInput = sortByClass inputData numAttributes	    
+            print sortedInput	
+        return ()
